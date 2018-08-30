71afcc5050edf8329ce21c92645c0adec27ef577
neha chopra <neha.chopra@paytm.com>
2018-08-30 12:21:49 +0530

    Customized consumer

---
+++ KafkaProducerConsumer/src/main/java/com/scotch/io/KafkaProducerConsumer/ProducerConsumerExamples/CustomSerializationExample/SupplierConsumer.java
@@ -0,0 +1,49 @@
+package com.scotch.io.KafkaProducerConsumer.ProducerConsumerExamples.CustomSerializationExample;
+
+import java.text.ParseException;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.assertj.core.util.Arrays;
+public class SupplierConsumer {
+
+	public static void main(String[] args) throws ParseException, InterruptedException, ExecutionException {
+		String topicName = "SupplierTopic";
+		String groupName = "SuppleirG";
+		
+		Properties properties = new Properties();
+		properties.put("bootstrap.servers", "localhost:9092, localhost:9093");
+		properties.put("group.id", groupName);
+		properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put("value.deserializer", "SupplierDeserializer");
+		
+		KafkaConsumer<String, Supplier> consumer = new KafkaConsumer<>(properties);
+		consumer.subscribe(Arrays.asList(topicName));///take a list of topic, regex and wildcard can be used
+		/*
+		 * telling consumer to read data from topic
+		 */
+		
+		while(true) {
+			ConsumerRecords<String, Supplier> records = consumer.poll(100);/// timeout 100 when no value is there
+			/*
+			 * poll method responsible for:
+			 * Connect to group coordinator
+			 * join the group
+			 * partition assignment 
+			 * sends heartbeat to tell it status
+			 * fetches the messages
+			 * 
+			 * On first time poll call, consumer connects with group coordinator, join the group, receive the partition assignment and then fetches the messages
+			 * Later poll sends the heartbeat to group coordinator to let know the status of the consumer, consumer should be quick in processing the data
+			 * else group coordinator will declare the consumer dead and will initiate the rebalance activity.
+			 */
+			for(ConsumerRecord<String, Supplier> record: records) {
+				 System.out.println("Supplier id= " + String.valueOf(record.value().getSupplierId()) + 
+						 " Supplier  Name = " + record.value().getSupplierName() + " Supplier Start Date = " + record.value().getSupplierStartDate().toString());
+			}
+		}
+	}
+}
